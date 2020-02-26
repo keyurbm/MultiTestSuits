121a1c248c5fe4e23b68516c6dd6ec98a78e54e4
Keyur Makwana <kaysmakwana@gmail.com>
2020-02-26 19:03:35 +0530

    Test

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	private String make;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(10 + 20 + "Javatpoint");   
+        System.out.println("Javatpoint" + 10 + 20);  
 	}
-
 }
